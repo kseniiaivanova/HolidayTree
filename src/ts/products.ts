531956b86065c8a4552fc61741b82e6ab6bbfc73
Kseniia Ivanova <kseniia.ivanova@gmail.com>
2022-12-20 23:05:46 +0100

    added description and title for products

--- src/ts/products.ts
+++ src/ts/products.ts
@@ -5,12 +5,18 @@ function createHTML(productlist: Product[]) {
   for (let i: number = 0; i < productlist.length; i++) {
     let container: HTMLDivElement = document.createElement("div");
     let imgTag: HTMLImageElement = document.createElement("img");
+    let title: HTMLHeadingElement = document.createElement("h3");
+    let descr: HTMLParagraphElement = document.createElement("p");
     let price: HTMLParagraphElement = document.createElement("p");
     imgTag.src = productlist[i].img;
+    title.innerHTML = productlist[i].name;
     price.innerHTML = productlist[i].price.toString();
+    descr.innerHTML = productlist[i].desc;
 
     container.appendChild(imgTag);
+    container.appendChild(title);
     container.appendChild(price);
+    container.appendChild(descr);
     document.body.appendChild(container);
   }
 }
