0878352d8d8d6283bb3d5b89b394f9842ef9230a
jessica tanmateos <jessica_tanmateps@hotmail.com>
2022-12-21 13:18:54 +0100

    changes of sidebar

--- src/ts/startpage.ts
+++ src/ts/startpage.ts
@@ -8,3 +8,27 @@ function init() {
 }
 
 init();
+
+function OpenCart() {
+  let startButton = document.getElementById("openCart") as HTMLButtonElement;
+  startButton.addEventListener("click", () => {
+    let openSidebar = document.getElementById("mySidebar") as HTMLDivElement;
+    openSidebar.style.width = "250px";
+    let openMain = document.getElementById("main") as HTMLDivElement;
+    openMain.style.marginLeft = "250px";
+  });
+}
+
+OpenCart();
+
+function closeCart() {
+  let closeButton = document.getElementById("closeCart") as HTMLButtonElement;
+  closeButton.addEventListener("click", () => {
+    let closeSidebar = document.getElementById("mySidebar") as HTMLDivElement;
+    closeSidebar.style.width = "0";
+    let closeMain = document.getElementById("main") as HTMLDivElement;
+    closeMain.style.marginLeft = "0";
+  });
+}
+
+closeCart();
