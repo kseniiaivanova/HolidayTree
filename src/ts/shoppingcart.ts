9e9a5982812869c14fbeb8f2bd13f138f5c7f061
annaboye <boye.anna@gmail.com>
2022-12-21 15:10:29 +0100

    added more functions to cart

--- src/ts/shoppingcart.ts
+++ src/ts/shoppingcart.ts
@@ -9,23 +9,43 @@ let shoppingCartItems: Product[] = [
     "kungsgran",
     "023",
     "lorem ipsum lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum",
-    0
+    1
   ),
 ];
 
+let newItem: Product = new Product(
+  "Kungsgran",
+  "En tät och ståtlig gran med oöverträfflig livslängd och svag citrusdoft.",
+  549,
+  "https://svenskagranar.se/wp-content/uploads/2021/09/produkt_kungsgran-1024x683.webp",
+  "kungsgran",
+  "023",
+  "lorem ipsum lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum",
+  0
+);
+
+addToCart(newItem);
+
 function addToCart(item: Product) {
-  shoppingCartItems.push(item);
+  item.amount++;
+  if (item.amount < 2) {
+    shoppingCartItems.push(item);
+  }
   shoppingCartHtml();
 }
 
 function removeFromCart(item: Product) {
-  let listindex = shoppingCartItems.indexOf(item);
-  shoppingCartItems.splice(listindex, 1);
+  item.amount--;
+  if (item.amount < 1) {
+    let listindex = shoppingCartItems.indexOf(item);
+    shoppingCartItems.splice(listindex, 1);
+  }
   shoppingCartHtml();
 }
 
 function shoppingCartHtml() {
   console.log("hello world");
+  // elements for the hole cart
 
   localStorage.setItem("cartlist", JSON.stringify(shoppingCartItems));
   let cartTag = document.getElementById("cart-container") as HTMLDivElement;
@@ -33,44 +53,69 @@ function shoppingCartHtml() {
   let totalAmountTag = document.createElement("p");
   totalAmountTag.className = "totalamount";
 
+  // a variable to be able to count
+
   let sum: number = 0;
-
   for (let i = 0; i < shoppingCartItems.length; i++) {
-    sum = sum + shoppingCartItems[i].price;
+    sum = sum + shoppingCartItems[i].amount * shoppingCartItems[i].price;
 
+    //create new element for each
+
+    let containerTag = document.createElement("div");
     let titleTag = document.createElement("h3");
+    let imgTag = document.createElement("img");
+    let changeContainer = document.createElement("div");
     let amountTag = document.createElement("p");
     let increaseBtn = document.createElement("i");
     let reduceBtn = document.createElement("i");
-    let containerTag = document.createElement("div");
-    let changeContainer = document.createElement("div");
-    let imgTag = document.createElement("img");
+    let priceTag = document.createElement("p");
     let deleteBtn = document.createElement("button");
 
-    imgTag.src = shoppingCartItems[i].img;
-    deleteBtn.innerHTML = "remove";
+    // innerhtml content
+
     titleTag.innerText = shoppingCartItems[i].name;
+
+    imgTag.src = shoppingCartItems[i].img;
+    imgTag.alt = shoppingCartItems[i].name;
+
+    deleteBtn.innerHTML = "remove";
     containerTag.className = "cart__item";
     changeContainer.className = "cart__item__changecontainer";
     amountTag.innerHTML = shoppingCartItems[i].amount.toString();
-    changeContainer.appendChild(increaseBtn);
-    changeContainer.appendChild(amountTag);
-    changeContainer.appendChild(reduceBtn);
-    containerTag.appendChild(titleTag);
-    containerTag.appendChild(imgTag);
-    containerTag.appendChild(changeContainer);
-    containerTag.appendChild(deleteBtn);
-
     increaseBtn.classList.add("bi", "bi-plus-square");
     reduceBtn.classList.add("bi", "bi-dash-square");
+
+    increaseBtn.addEventListener("click", () => {
+      addToCart(shoppingCartItems[i]);
+    });
+
+    reduceBtn.addEventListener("click", () => {
+      removeFromCart(shoppingCartItems[i]);
+    });
 
     deleteBtn.classList.add("buttons");
     deleteBtn.addEventListener("click", () => {
       removeFromCart(shoppingCartItems[i]);
     });
+    priceTag.innerHTML =
+      (shoppingCartItems[i].amount * shoppingCartItems[i].price).toString() +
+      "sek";
+
+    //to display
+
+    changeContainer.appendChild(reduceBtn);
+    changeContainer.appendChild(amountTag);
+    changeContainer.appendChild(increaseBtn);
+
+    containerTag.appendChild(titleTag);
+    containerTag.appendChild(imgTag);
+    containerTag.appendChild(changeContainer);
+    containerTag.appendChild(priceTag);
+    containerTag.appendChild(deleteBtn);
 
     cartTag.appendChild(containerTag);
   }
+  //to display total price
   totalAmountTag.innerHTML = "Totalt: " + sum.toString() + " sek";
   cartTag.appendChild(totalAmountTag);
 }
